eb5ca21b81cee6c67d7ae2f0dc2b9305e47130ac
emperorbj <cwaku96@gmail.com>
2024-08-17 16:06:12 +0100

    "Added dotenv dependency, updated .gitignore, and refactored index.js to use express, cors, and mongoose"

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hello there')
+const express = require('express');
+const app = express();
+const port = 3000;
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+
+// connection
+mongoose.connect(process.env.MONGODB_CONNECTION_URI)
+.then(()=>{
+    console.log('Connected to MongoDB');
+    app.listen(port,()=>{
+        console.log(`Server is running on port ${port}`)
+    })
+})
+.catch(()=>{
+    console.log('Error connecting to MongoDB');
+})
