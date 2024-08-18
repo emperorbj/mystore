53320cf40e14e6b28ec5f9adcbdefd9d9e44ae4a
emperorbj <cwaku96@gmail.com>
2024-08-18 16:35:07 +0100

    "Updated products controller to handle sorting and selecting, added create and update product functionality, and modified product route to include new endpoints."

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -4,7 +4,7 @@ const Products = require('../models/products.model');
 
 const getAllProducts = async (req,res) => {
     try{
-        const { company,name } = req.query;
+        const { company,name,featured,sort, select } = req.query;
         const queryObject = {};
         if(company){
             queryObject.company = {$regex: company, $options: 'i'};
@@ -19,7 +19,24 @@ const getAllProducts = async (req,res) => {
         if(featured){
             queryObject.featured = featured;
         }
-        const products = await Products.find(queryObject);
+
+        let apiData = Products.find(queryObject);
+
+
+        if(sort){
+            let sortFix = sort.replace(","," ");
+            queryObject.sort = sortFix;
+            apiData = apiData.sort(sortFix);
+        }
+
+
+        if(select){
+            let selectFix = select.split(",").join(" ");
+            queryObject.select = selectFix;
+            apiData = apiData.select(selectFix);
+        }
+
+        const products = await apiData;
         res.status(200).json(products);
     }
     catch(error){
@@ -43,7 +60,39 @@ const getProductById = async (req,res) => {
     }
 }
 
+
+const addNewProduct = async (req,res) =>{
+    try{
+        const product = await Products.create(req.body);
+        res.status(201).json(product);
+
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+}
+
+
+const updateProduct = async (req,res) => {
+    try{
+        const id = req.params.id
+        const product = await Products.findByIdAndUpdate(id, req.body)
+
+        if(!product){
+            return res.status(404).json({message: "Product not found"})
+        }
+
+        const updatedProduct = await Products.findById(id)
+        res.status(200).json(updatedProduct)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+}
+
 module.exports = {
     getAllProducts,
-    getProductById
+    getProductById,
+    addNewProduct,
+    updateProduct
 }
